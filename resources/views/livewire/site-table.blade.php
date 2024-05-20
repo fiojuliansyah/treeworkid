96c569913ad7d00f1c242a1e8ab66b9bd6b26835
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-05-20 17:30:56 +0700

    update search

--- resources/views/livewire/site-table.blade.php
+++ resources/views/livewire/site-table.blade.php
@@ -3,7 +3,7 @@
         <div class="card-title">
             <div class="d-flex align-items-center position-relative my-1">
                 <i class="ki-outline ki-magnifier fs-3 position-absolute ms-5"></i>
-                <input type="text" wire:model.live="search" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Search company" />
+                <input type="text" wire:model.live="search" data-kt-user-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Search site" />
             </div>
         </div>
         <div class="card-toolbar">
