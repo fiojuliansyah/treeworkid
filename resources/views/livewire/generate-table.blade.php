1013cb94b821347c3cb75289312b5b180270700f
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-05-22 12:48:04 +0700

    Update generate-table.blade.php

--- resources/views/livewire/generate-table.blade.php
+++ resources/views/livewire/generate-table.blade.php
@@ -93,7 +93,7 @@
                         <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
                             <!--begin::Menu item-->
                             <div class="menu-item px-3">
-                                <a href="{{ route('letter-regenerate', ['generate' => $generate->id]) }}" class="menu-link px-3">Regenerate</a>
+                                <a href="{{ route('letter-regenerate', ['generate' => $generate->id]) }}" target="_blank" class="menu-link px-3">Regenerate</a>
                             </div>
                             <!--end::Menu item-->
                             <!--begin::Menu item-->
