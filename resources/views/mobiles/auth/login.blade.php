46332a15df59ed1eb26438991624029534838b9d
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2024-09-05 19:35:15 +0700

    Update login.blade.php

--- resources/views/mobiles/auth/login.blade.php
+++ resources/views/mobiles/auth/login.blade.php
@@ -12,7 +12,7 @@
         <form id="login-form" class="form w-100" method="POST" action="{{ route('login') }}">
             @csrf
             <div class="input-style no-borders has-icon validate-field mb-4">
-                <input type="text" name="login" class="form-control validate-name" placeholder="Email atau Employee NIK">
+                <input type="text" name="login" class="form-control validate-name" placeholder="Email atau Employee NIK" oninput="this.value = this.value.toLowerCase()">
                 <label class="color-blue-dark font-10 mt-1">Email atau Employee NIK</label>
                 @error('login')
                 <em style="color: red">{{ $message }}</em>
