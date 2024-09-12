91d975da7d88c9968322ea70be1a30b6cd0c07a5
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2024-09-12 15:19:31 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,26 +26,29 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+
+        if (!$this->app->runningInConsole()) {
             
-        config(['app.locale' => 'id']);
-        Carbon::setLocale('id');
+            config(['app.locale' => 'id']);
+	        Carbon::setLocale('id');
 
-        $statuses = Status::all();
-        view()->share('statuses', $statuses);
+            $statuses = Status::all();
+            view()->share('statuses', $statuses);
 
-        $countPendingAll = Applicant::where('approve_id', null)
-            ->whereNull('done')
-            ->count();
-        view()->share('countPendingAll', $countPendingAll);
+            $countPendingAll = Applicant::where('approve_id', null)
+                ->whereNull('done')
+                ->count();
+            view()->share('countPendingAll', $countPendingAll);
 
-        $countPending = Applicant::where('approve_id', null)
-            ->where('status_id', '=', 0)
-            ->whereNull('done')
-            ->count();
-        view()->share('countPending', $countPending);
+            $countPending = Applicant::where('approve_id', null)
+                ->where('status_id', '=', 0)
+                ->whereNull('done')
+                ->count();
+            view()->share('countPending', $countPending);
 
-        $general = Company::where('is_default', 1)
-            ->first();
-        view()->share('general', $general);
+            $general = Company::where('is_default', 1)
+                ->first();
+            view()->share('general', $general);
+        }
     }
 }
