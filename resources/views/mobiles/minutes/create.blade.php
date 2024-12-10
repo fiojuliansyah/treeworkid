3f736c77090b4b605891dbfab469548a206846f5
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2024-12-10 15:15:43 +0700

    Update create.blade.php

--- resources/views/mobiles/minutes/create.blade.php
+++ resources/views/mobiles/minutes/create.blade.php
@@ -54,12 +54,18 @@
         <a href="#" onclick="submitFormWithLocation(event)" class="btn btn-full btn-m rounded-s text-uppercase font-900 shadow-xl bg-highlight" id="submitButton">
             Submit
         </a>
-        <div id="loadingSpinner" class="d-none">
-            <i class="fa fa-spinner fa-spin"></i> Loading...
-        </div>
+
+        <!-- Spinner - Hidden by default -->
     </div>
+    <div id="loadingSpinner" class="d-none spinner" style="border: 8px solid #f3f3f3; border-top: 8px solid #00B5CC; border-radius: 50%; width: 60px; height: 60px; animation: spin 1s linear infinite;"></div>
 </div>
 
+<style>
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
 @endsection
 
 @push('js')
